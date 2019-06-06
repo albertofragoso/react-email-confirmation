b7f8a5590a7c29d4a539c8f5488da2af6ef54592
albertofragoso <hola@albertofragoso.com>
2019-06-06 14:58:42 -0500

    Agregando Context

--- client/src/Router.js
+++ client/src/Router.js
@@ -2,15 +2,28 @@ import React from 'react'
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import Landing from './components/Landing'
 import Confirm from './components/Confirm'
+import Spinner from './components/Spinner'
+import { Mycontext } from './context';
 
 const Router = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path='/' component={Landing} />
-      <Route exact path='/confirm/:id' component={Confirm} />
-      <Redirect from='*' to='/' />
-    </Switch>
-  </BrowserRouter>
+  <Mycontext.Consumer>
+    {
+      ({ loading }) => {
+
+        if(loading) return <Spinner size='8x' spinnign='spinning' />
+
+        return (
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/confirm/:id' component={Confirm} />
+            <Redirect from='*' to='/' />
+          </Switch>
+        </BrowserRouter>
+        )
+      } 
+    }
+  </Mycontext.Consumer>
 )
 
 export default Router
---
+++ client/src/context/index.js
@@ -0,0 +1,29 @@
+import React, { createContext, Component } from 'react'
+import { API_URL } from './config'
+
+export const Mycontext = createContext()
+
+class MyProvider extends Component {
+  state = {
+    loading: true
+  }
+
+  componentDidMount = () => {
+   fetch(`${API_URL}/wake-up`)
+    .then(res => res.json())
+    .then(() => this.setState({ loading: false }))
+    .catch(err => console.log(err))
+  }
+
+  render() {
+    return(
+      <Mycontext.Provider value={{
+        loading: this.state.loading
+      }}>
+        {this.props.children}
+      </Mycontext.Provider>
+    )
+  }
+}
+
+export default MyProvider
